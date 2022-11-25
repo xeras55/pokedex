3aae23e5f58619d120d37efc890c938d6c02d9ad
xeras55 <114122029+xeras55@users.noreply.github.com>
2022-11-25 13:27:45 -0600

    ya jalo nomas faltan detalles de colores pero ya quedo

--- pokedex.js
+++ pokedex.js
@@ -1,4 +1,28 @@
 
+
+const validar = () => {
+    if( document.getElementById('colorofP0')) {
+        console.log('aaaaa1');
+        validar1();
+    }else {
+        console.log('aaaaa2');
+        fetchPokemon();
+        }
+}
+
+const validar1 = () => {
+    x();
+
+}
+
+// const validar = () => {
+//     x();
+    
+// }
+
+
+
+
 const fetchPokemon = () => {
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
@@ -6,7 +30,7 @@ const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
-            console.log(res);
+            // console.log(res);
             pokeImage("./pikachu-sad.gif")
         }
         else { 
@@ -14,8 +38,8 @@ const fetchPokemon = () => {
         }
     }).then((data) => {
         if (data) {
-            //console.log(data);
-            let pokeImageContainer = data.sprites.front_shiny;
+            console.log(data);
+            let pokeImageContainer = data.sprites.front_default;
             let abilities = data.abilities;
             let move = data.moves;
             let type = data.types;
@@ -25,12 +49,20 @@ const fetchPokemon = () => {
             let pokeWi = data.weight;
             let hpBase = data.stats;
             let attackBase = data.stats;
+            let defenseBase = data.stats;
+            let specialAttackBase = data.stats;
+            let specialDefenseBase = data.stats;
+            let speedBase = data.stats;
+            pokeSpeed(speedBase);
+            pokeSpDf(specialDefenseBase);
+            pokeSpAt(specialAttackBase);
             pokeAta(attackBase);
+            pokeDef(defenseBase);
             pokeHp(hpBase);
             pokeTypes(type);
             pokeHeight(pokeHe);
             pokeWeight(pokeWi);
-            // //pokeMoves(move);
+            pokeMoves(move);
             // pokeData(abilities);
             pokeImage(pokeImageContainer);
             nameOfPoke(pokeNames);
@@ -44,6 +76,7 @@ const fetchPokemon = () => {
 //     const pokePhoto = document.getElementById("pokeImg");
 //     pokePhoto.src = url;
 // }
+//------------------añadir todos los valores del poke----------------------------------------
 const pokeImage = (url) => {
     const pokePhoto = document.getElementById("pokeImageContainer");
     const li = document.createElement('img');
@@ -97,22 +130,79 @@ const pokeWeight = (weight) => {
 
 const pokeHp = (stats) => {
     // get's HP of the poke 
-    const pokeHP = document.getElementById("hpBase");
-    const hpData = stats.map((item1) => item1.base_stat);
+    const pokeHP1 = document.getElementById("containH");
+    const hpData = stats.map((item) => item.base_stat);
     // console.log(hpData);
     let theOne = hpData[0];
     //console.log(theOne);
     const createHp = document.createElement('p');
     createHp.id = 'colorOfHp';
     createHp.textContent = theOne;
-    pokeHP.appendChild(createHp);
+    pokeHP1.appendChild(createHp);
 }
 const pokeAta = (stats) => {
-    const pokeAtt = document.getElementById("attackBase");
-    const attackData = stats.map((item1) => item1.base_stat);
-    let theOneA = attackData[1]
+    const pokeAtt = document.getElementById("containA");
+    const attackData = stats.map((item) => item.base_stat);
+    let theOneA1 = attackData[1]
+    const createAtt = document.createElement('p');
+    createAtt.id = 'colorOfAtt';
+    createAtt.textContent = theOneA1;
+    pokeAtt.appendChild(createAtt);
 }
 
+const pokeDef = (stats) => {
+    const pokeDeff = document.getElementById("containD");
+    const deffData = stats.map((item) => item.base_stat);
+    let theOneD1 = deffData[2]
+    const createDeff = document.createElement('p');
+    createDeff.id = 'colorOfDeff';
+    createDeff.textContent = theOneD1;
+    pokeDeff.appendChild(createDeff);
+}
+
+
+const pokeSpAt = (stats) => {
+    const pokeSpA = document.getElementById("containSa");
+    const spAtData = stats.map((item) => item.base_stat);
+    let theOneSpA1 = spAtData[3]
+    const createSpAt = document.createElement('p');
+    createSpAt.id = 'colorOfSpAt';
+    createSpAt.textContent = theOneSpA1;
+    pokeSpA.appendChild(createSpAt);
+}
+const pokeSpDf = (stats) => {
+    const pokeSpD = document.getElementById("containSd");
+    const spDfData = stats.map((item) => item.base_stat);
+    let theOneSpD1 = spDfData[4]
+    const createSpDf = document.createElement('p');
+    createSpDf.id = 'colorOfSpDf';
+    createSpDf.textContent = theOneSpD1;
+    pokeSpD.appendChild(createSpDf);
+}
+
+const pokeSpeed = (stats) => {
+    const pokeSpe = document.getElementById("containSp");
+    const speedData = stats.map((item) => item.base_stat);
+    let theOneSpee1 = speedData[5]
+    const createSpee = document.createElement('p');
+    createSpee.id = 'colorOfSpee';
+    createSpee.textContent = theOneSpee1;
+    pokeSpe.appendChild(createSpee);
+}
+
+
+const pokeMoves = (moves) => {
+    const pokeMoves = document.getElementById("move");
+    const movesName = moves.map((item) => item.move.name);
+    createMov = document.createElement('p');
+    createMov.id = 'colorOFMove';
+    createMov.textContent = movesName;
+    pokeMoves.appendChild(createMov);
+    move.style.visibility = "hidden"; 
+
+}
+
+//----------------------- funciones para calcuilar el peso y medida del poke -----------------------------------------
 const convertH = (heightData) => {
     let result = heightData / 10;
     return result;
@@ -122,6 +212,74 @@ const convertW = (weightData) => {
     let resultW = weightData / 10;
     return resultW;
 } 
+//-----------------------------esconder los stats para mostrar los movimientos----------------------
+const hidde = () => {
+    const hidde1 = document.getElementById('statsBase');
+    const hiddeB = document.getElementById('natureType');
+    hiddeB.style.visibility = "hidden"; 
+    hidde1.style.visibility = "hidden"; 
+    const unHidde = document.getElementById('move');
+    unHidde.style.visibility = ""; 
+}
+
+//---------------------------------reaparecer los stats y esconder los movimientos--------------------------
+const showAgain = () => {
+    const unhidde1 = document.getElementById('statsBase');
+    const unhiddeB = document.getElementById('natureType');
+    unhiddeB.style.visibility = ""; 
+    unhidde1.style.visibility = ""; 
+    const Hidde = document.getElementById('move');
+    Hidde.style.visibility = "hidden"; 
+}
+
+
+//---------------------------------------------------------------------------
+//----------------------------search again--------------------------------------
+
+
+
+const x = () => {
+    const clearNumber = document.getElementById('pokeId');
+    const clearName = document.getElementById('pokeNames');
+    const clearNature = document.getElementById('natureType');
+    const clearStatsHp = document.getElementById('containH');
+    const clearStatsAttack = document.getElementById('containA');
+    const clearStatsDefense = document.getElementById('containD');
+    const clearStatsSpeAta = document.getElementById('containSa');
+    const clearStatsSpeDef = document.getElementById('containSd');
+    const clearStatsSpeed = document.getElementById('containSp');
+    const clearMove = document.getElementById('move');
+    const clearWeig = document.getElementById('containerStats2');
+    const clearHeig = document.getElementById('containerStats1');
+    
+    clearNumber.textContent ='';
+    clearName.textContent ='';
+    clearNature.textContent ='';
+    clearStatsHp.textContent ='';
+    clearStatsAttack.textContent ='';
+    clearStatsDefense.textContent ='';
+    clearStatsSpeAta.textContent ='';
+    clearStatsSpeDef.textContent ='';
+    clearStatsSpeed.textContent ='';
+    clearName.textContent ='';
+    clearMove.textContent = '';
+    borrarAlv;
+    clearWeig.textContent = '';
+    clearHeig.textContent = '';
+    var d = document.getElementById('pokeImageContainer');
+    var d_nested = document.getElementById('colorOfImg');
+    var borrarAlv = d.removeChild(d_nested);
+    fetchPokemon();
+    console.log('asdasd');
+    
+}
+
+//------------------------------------------------------------------------------
+// const elem = document.getElementById('.natureType').style.visibility = "hidden"; 
+// const btnHidd = document.querySelector('.lineDer1');
+//     btnHidd.addEventListener("click", () => {
+//     elem;
+//     });
 
 // const pokeData = (abilities) =>{
 //     const pokeAbilities = document.getElementById("abilities");
@@ -130,11 +288,7 @@ const convertW = (weightData) => {
 //     pokeAbilities.innerHTML =  abilitiesName;
 // }
 
-// // const pokeMoves = (moves) => {
-// //     const pokeMoves = document.getElementById("move");
-// //     const movesName = moves.map((item) => item.move.name);
-// //     pokeMoves.innerHTML = movesName;
-// // }
+
 
 
 // const pokeTypes = (types) => {
--- test.js
+++ test.js
@@ -1,8 +1,110 @@
-console.log('Hola weones');
+// console.log('Hola weones');
 
 
-let convertir = (n) => {
-    let resultado = n * 10;
-    return resultado;
+// let convertir = (n) => {
+//     let resultado = n * 10;
+//     return resultado;
+// }
+// console.log(convertir(10));
+
+
+
+
+
+
+
+const validar = () => {
+    const validacion = document.getElementById('pokeNames');
+    let valid = validacion.value;
+    if( valid == true ) {
+        fetchPokemon();
+    }else {
+        validar1();
+        }
 }
-console.log(convertir(10));
+
+
+
+
+// const validar = () => {
+//     x();
+
+// }
+
+const validar1 = () => {
+    x();
+
+}
+
+
+
+
+
+
+
+
+
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./pikachu-sad.gif")
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            let move = data.moves;
+            pokeMoves(move);
+            let pokeNames = data.name;
+            nameOfPoke(pokeNames);
+            let pokeWi = data.weight;
+            pokeWeight(pokeWi);
+        }
+    });
+}
+
+
+const nameOfPoke = (name) => {
+    const pkname = document.getElementById("pokeNames");
+    const names = name;
+    const createpknm = document.createElement('p');
+    createpknm.id = "colorofP0";
+    createpknm.textContent = names.toUpperCase();
+    pkname.appendChild(createpknm);
+    // console.log(name);
+}
+
+const pokeMoves = (moves) => {
+    const pokeMoves = document.getElementById("move");
+    const movesName = moves.map((item) => item.move.name);
+    createMov = document.createElement('p');
+    createMov.id = 'colorOFMove';
+    createMov.textContent = movesName;
+    pokeMoves.appendChild(createMov);
+
+}
+
+const x = () => {
+    const clearName = document.getElementById('pokeNames');
+    const clearMove = document.getElementById('move');
+    const clearwei = document.getElementById('containerStats2');
+    clearName.textContent ='';
+    clearMove.textContent = '';
+    clearwei.textContent = '';
+    fetchPokemon();
+}
+
+const pokeWeight = (weight) => {
+    const pokeW = document.getElementById("containerStats2");
+    const weightData = weight;
+    const createWeight = document.createElement('p');
+    createWeight.id = 'colorOfWe';
+    createWeight.textContent = weightData;
+    pokeW.appendChild(createWeight);
+}
